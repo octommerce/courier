31c436b4bd81c9c13b89d139092bb22f029c0a4b
Surahman <surahman.duang@gmail.com>
2018-03-26 16:24:11 +0700

    Generate awb

--- couriers/Jne.php
+++ couriers/Jne.php
@@ -108,7 +108,53 @@ class Jne extends Courier
 
     public function generateAwb($data) 
     {
-    
+        try {
+            $url = "http://apiv2.jne.co.id:10102/tracing/api/generatecnote";
+
+            $response = Http::post($url, function($http) use ($data) {
+                $http->data('username', $this->getUsername());
+                $http->data('api_key', $this->getApiKey());
+                $http->data('OLSHOP_BRANCH', 'CGK000');
+                $http->data('OLSHOP_CUST', $data['OLSHOP_CUST']);
+                $http->data('OLSHOP_ORDERID', $data['OLSHOP_ORDERID']);
+                $http->data('OLSHOP_SHIPPER_NAME', $data['OLSHOP_SHIPPER_NAME']);
+                $http->data('OLSHOP_SHIPPER_ADDR1', $data['OLSHOP_SHIPPER_ADDR1']);
+                $http->data('OLSHOP_SHIPPER_ADDR2', $data['OLSHOP_SHIPPER_ADDR2']);
+                $http->data('OLSHOP_SHIPPER_CITY', $data['OLSHOP_SHIPPER_CITY']);
+                $http->data('OLSHOP_SHIPPER_REGION', $data['OLSHOP_SHIPPER_REGION']);
+                $http->data('OLSHOP_SHIPPER_ZIP', $data['OLSHOP_SHIPPER_ZIP']);
+                $http->data('OLSHOP_SHIPPER_PHONE', $data['OLSHOP_SHIPPER_PHONE']);
+
+                $http->data('OLSHOP_RECEIVER_NAME', $data['OLSHOP_RECEIVER_NAME']);
+                $http->data('OLSHOP_RECEIVER_ADDR1', $data['OLSHOP_RECEIVER_ADDR1']);
+                $http->data('OLSHOP_RECEIVER_ADDR2', $data['OLSHOP_RECEIVER_ADDR2']);
+                $http->data('OLSHOP_RECEIVER_CITY', $data['OLSHOP_RECEIVER_CITY']);
+                $http->data('OLSHOP_RECEIVER_REGION', $data['OLSHOP_RECEIVER_REGION']);
+                $http->data('OLSHOP_RECEIVER_ZIP', $data['OLSHOP_RECEIVER_ZIP']);
+                $http->data('OLSHOP_RECEIVER_PHONE', $data['OLSHOP_RECEIVER_PHONE']);
+
+                $http->data('OLSHOP_QTY', $data['OLSHOP_QTY']);
+                $http->data('OLSHOP_WEIGHT', $data['OLSHOP_WEIGHT']);
+                $http->data('OLSHOP_GOODSDESC', $data['OLSHOP_GOODSDESC']);
+                $http->data('OLSHOP_GOODSVALUE', $data['OLSHOP_GOODSVALUE']);
+                $http->data('OLSHOP_GOODSTYPE', $data['OLSHOP_GOODSTYPE']);
+                $http->data('OLSHOP_INST', $data['OLSHOP_INST']);
+                $http->data('OLSHOP_INS_FLAG', $data['OLSHOP_INS_FLAG']);
+                $http->data('OLSHOP_ORIG', $data['OLSHOP_ORIG']);
+                $http->data('OLSHOP_DEST', $data['OLSHOP_DEST']);
+                $http->data('OLSHOP_SERVICE', $data['OLSHOP_SERVICE']);
+                $http->data('OLSHOP_COD_FLAG', $data['OLSHOP_COD_FLAG']);
+                $http->data('OLSHOP_COD_AMOUNT', $data['OLSHOP_COD_AMOUNT']);
+            });
+
+            if ($response->code != 200) {
+                throw new Exception($this->getErrorMessage($response));
+            }
+        }catch(Exception $e){
+            throw new SystemException($e->getMessage());
+        }
+
+        return $response->body;
     }
 
     public function availableServices()
